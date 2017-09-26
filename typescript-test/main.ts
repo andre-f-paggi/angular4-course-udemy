ae6a512e28b0e7ae1827dec8a43eb2ce0f09721d
Andre Franciscato Paggi <andre.paggi@corp.nova>
2017-09-26 16:56:44 -0300

    Typescript: Types

--- typescript-test/main.ts
+++ typescript-test/main.ts
@@ -1,8 +1,30 @@
+// type error
+let count = 5;
+count = 'a';
 
-function log(message){
-	console.log(message);
-}
+// variable not assigned. Type equals 'any'
+let a;
+a = 1;
+a = 'abc';
+a = true;
+a = {};
 
-var message = "Hello World!";
+// specifying type without initialing
+let b: number;
+b = 1;
+b = 'abc';
+b = true;
+b = {};
 
-log(message);
+// types of variables
+let a1: number;
+let a2: boolean;
+let a3: string;
+let a4: any;
+let a5: number[] = [1,2,3];
+let a6: any[] = [1,true,'a', false];
+
+// enum
+// enum Color { Red, Green, Blue};
+enum Color { Red = 0, Green = 1, Blue = 2}; // specify value to avoid errors due to future addition of values
+let backgroundColor = Color.Red;
