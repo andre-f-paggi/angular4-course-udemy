ee59a3624666dbc2d3e2d919e4e3df117e3832b3
Andre Franciscato Paggi <andre.paggi@corp.nova>
2017-09-26 17:22:38 -0300

    Typescript: Type Assertions

--- typescript-test/main.ts
+++ typescript-test/main.ts
@@ -1,30 +1,10 @@
-// type error
-let count = 5;
-count = 'a';
+// type assetions
 
-// variable not assigned. Type equals 'any'
-let a;
-a = 1;
-a = 'abc';
-a = true;
-a = {};
+let message;
+message = 'abc';
 
-// specifying type without initialing
-let b: number;
-b = 1;
-b = 'abc';
-b = true;
-b = {};
+// 1 - Preferable way
+let endsWithC1 = (<string>message).endsWith('c');
 
-// types of variables
-let a1: number;
-let a2: boolean;
-let a3: string;
-let a4: any;
-let a5: number[] = [1,2,3];
-let a6: any[] = [1,true,'a', false];
-
-// enum
-// enum Color { Red, Green, Blue};
-enum Color { Red = 0, Green = 1, Blue = 2}; // specify value to avoid errors due to future addition of values
-let backgroundColor = Color.Red;
+// 2 - Another way
+let endsWithC2 = (message as string).endsWith('c');
