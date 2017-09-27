cd1613258c43c164d85a4e6b715bf3927f087f9e
Andre Franciscato Paggi <andre.paggi@corp.nova>
2017-09-27 16:00:42 -0300

    32. Directives

--- angular4-project/src/app/courses.component.ts
+++ angular4-project/src/app/courses.component.ts
@@ -2,8 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-courses',
-  template: '<h2>{{ "Title: " + title }}</h2>'
+  template: `
+    <h2>{{ "Title: " + title }}</h2>
+    <ul>
+      <li *ngFor="let course of courses">
+       {{ course }}
+      </li>
+    </ul>
+  `
+  // "*" prefix means that the following directive that changes the structure of the DOM
 })
 export class CoursesComponent {
   title = 'List of courses';
+  courses = ['Course 1', 'Course 2', 'Course 3'];
 }
