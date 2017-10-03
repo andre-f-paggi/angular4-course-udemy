bbe7787bcee10e8c051d847ac4a221d9fc2748ae
Andre Franciscato Paggi <andre.paggi@corp.nova>
2017-10-03 13:41:09 -0300

    93. Asynchronous Operations

--- angular4-project/src/app/signup-form/username.validators.ts
+++ angular4-project/src/app/signup-form/username.validators.ts
@@ -1,3 +1,4 @@
+import { setTimeout } from 'timers';
 import { AbstractControl, ValidationErrors } from '@angular/forms';
 
 export class UsernameValidators {
@@ -5,6 +6,17 @@ export class UsernameValidators {
     if ((control.value as string).indexOf(' ') >= 0) {
       return { cannotContainSpace : true };
     }
+
+    return null;
+  }
+
+  static shouldBeUnique(control: AbstractControl): ValidationErrors | null {
+    setTimeout(() => {
+      if (control.value === 'mosh') {
+        return { shouldBeUnique: true };
+      }
+      return null;
+    }, 2000);
 
     return null;
   }
